fa29c967324e4adbc5edad6b84e4640834883441
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2023-08-07 14:48:33 +0700

    Add: App Themes

---
+++ lib/themes/app_themes.dart
@@ -0,0 +1,20 @@
+part of 'themes.dart';
+
+double defaultMargin = 16;
+double defaultRadius = 11;
+
+//Font Weight
+FontWeight lightWeight = FontWeight.w300;
+FontWeight regularWeight = FontWeight.w400;
+FontWeight mediumWeight = FontWeight.w500;
+FontWeight semiBoldWeight = FontWeight.w600;
+FontWeight boldWeight = FontWeight.w700;
+FontWeight extraBoldWeight = FontWeight.w800;
+FontWeight blackWeight = FontWeight.w900;
+
+//Text Style
+TextStyle defaultTextStyle = GoogleFonts.poppins().copyWith(
+  fontSize: 14,
+  color: AppColor.blackColor(),
+  fontWeight: regularWeight,
+);
---
+++ lib/themes/themes.dart
@@ -0,0 +1,5 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sneakers_dept/export.dart';
+import 'package:sneakers_dept/themes/color_themes.dart';
+
+part 'app_themes.dart';
