eb804235e1e74be9eccd951270ad4500b8b875c8
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2023-08-07 16:25:37 +0700

    Add: bottom info in home screen

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -18,161 +18,194 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(30),
-          child: Column(
-            children: [
-              //Header
-              // const Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //   children: [
-              //     Column(
-              //       children: [
-              //         Text('Hallo, Alex'),
-              //         Text('@alexkeinn'),
-              //       ],
-              //     ),
-              //     CircleAvatar()
-              //   ],
-              // ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                //Header
+                // const Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //   children: [
+                //     Column(
+                //       children: [
+                //         Text('Hallo, Alex'),
+                //         Text('@alexkeinn'),
+                //       ],
+                //     ),
+                //     CircleAvatar()
+                //   ],
+                // ),
 
-              //Filter
-              // Row(
-              //   children: [
-              //     Container(
-              //       margin: EdgeInsets.symmetric(
-              //         horizontal: 10,
-              //       ),
-              //       width: 80,
-              //       height: 40,
-              //       color: AppColor.priceColor(),
-              //       child: Center(
-              //         child: Text(
-              //           'All Shoes',
-              //         ),
-              //       ),
-              //     ),
-              //     Container(
-              //       margin: EdgeInsets.symmetric(
-              //         horizontal: 10,
-              //       ),
-              //       width: 80,
-              //       height: 40,
-              //       color: AppColor.grey1Color(),
-              //       child: Center(
-              //         child: Text(
-              //           'Running',
-              //         ),
-              //       ),
-              //     ),
-              //     Container(
-              //       margin: EdgeInsets.symmetric(
-              //         horizontal: 10,
-              //       ),
-              //       width: 80,
-              //       height: 40,
-              //       color: AppColor.grey1Color(),
-              //       child: Center(
-              //         child: Text(
-              //           'Training',
-              //         ),
-              //       ),
-              //     ),
-              //   ],
-              // ),
-              CustomTextField(
-                backgroundColor: AppColor.grey4Color(),
-                hintText: 'Search Location',
-                suffixIcon: Icon(Icons.search),
-              ),
-              Column(
-                children: [
-                  Icon(
-                    Icons.cloud,
-                    size: 120,
-                  ),
-                  Text(
-                    'Hyderabad',
-                    style: mainInfoTextStyle,
-                  ),
-                  Text(
-                    '31°',
-                    style: mainInfoTextStyle.copyWith(
-                      fontSize: 70,
-                    ),
-                  ),
-                ],
-              ),
-              CustomContainer(
-                padding: EdgeInsets.symmetric(
-                  horizontal: defaultMargin,
-                  vertical: defaultMargin,
+                //Filter
+                // Row(
+                //   children: [
+                //     Container(
+                //       margin: EdgeInsets.symmetric(
+                //         horizontal: 10,
+                //       ),
+                //       width: 80,
+                //       height: 40,
+                //       color: AppColor.priceColor(),
+                //       child: Center(
+                //         child: Text(
+                //           'All Shoes',
+                //         ),
+                //       ),
+                //     ),
+                //     Container(
+                //       margin: EdgeInsets.symmetric(
+                //         horizontal: 10,
+                //       ),
+                //       width: 80,
+                //       height: 40,
+                //       color: AppColor.grey1Color(),
+                //       child: Center(
+                //         child: Text(
+                //           'Running',
+                //         ),
+                //       ),
+                //     ),
+                //     Container(
+                //       margin: EdgeInsets.symmetric(
+                //         horizontal: 10,
+                //       ),
+                //       width: 80,
+                //       height: 40,
+                //       color: AppColor.grey1Color(),
+                //       child: Center(
+                //         child: Text(
+                //           'Training',
+                //         ),
+                //       ),
+                //     ),
+                //   ],
+                // ),
+                CustomTextField(
+                  backgroundColor: AppColor.grey4Color(),
+                  hintText: 'Search Location',
+                  suffixIcon: Icon(Icons.search),
                 ),
-                containerType: RoundedContainerType.noOutline,
-                radius: defaultRadius,
-                backgroundColor: AppColor.grey4Color(),
-                width: double.infinity,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Column(
                   children: [
-                    Column(
-                      children: [
-                        Text(
-                          'TIME',
-                          style: defaultTextStyle,
-                        ),
-                        Text(
-                          '11:25 AM',
-                          style: defaultTextStyle.copyWith(
-                            fontWeight: boldWeight,
-                          ),
-                        ),
-                      ],
+                    Icon(
+                      Icons.cloud,
+                      size: 120,
                     ),
-                    Column(
-                      children: [
-                        Text(
-                          'UV',
-                          style: defaultTextStyle,
-                        ),
-                        Text(
-                          '4',
-                          style: defaultTextStyle.copyWith(
-                            fontWeight: boldWeight,
-                          ),
-                        ),
-                      ],
+                    Text(
+                      'Hyderabad',
+                      style: mainInfoTextStyle,
                     ),
-                    Column(
-                      children: [
-                        Text(
-                          '% RAIN',
-                          style: defaultTextStyle,
-                        ),
-                        Text(
-                          '58 %',
-                          style: defaultTextStyle.copyWith(
-                            fontWeight: boldWeight,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          'AQ',
-                          style: defaultTextStyle,
-                        ),
-                        Text(
-                          '22',
-                          style: defaultTextStyle.copyWith(
-                            fontWeight: boldWeight,
-                          ),
-                        ),
-                      ],
+                    Text(
+                      '31°',
+                      style: mainInfoTextStyle.copyWith(
+                        fontSize: 70,
+                      ),
                     ),
                   ],
                 ),
-              )
-            ],
+                CustomContainer(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: defaultMargin,
+                    vertical: defaultMargin,
+                  ),
+                  containerType: RoundedContainerType.noOutline,
+                  radius: defaultRadius,
+                  backgroundColor: AppColor.grey4Color(),
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            'TIME',
+                            style: defaultTextStyle,
+                          ),
+                          Text(
+                            '11:25 AM',
+                            style: defaultTextStyle.copyWith(
+                              fontWeight: boldWeight,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'UV',
+                            style: defaultTextStyle,
+                          ),
+                          Text(
+                            '4',
+                            style: defaultTextStyle.copyWith(
+                              fontWeight: boldWeight,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            '% RAIN',
+                            style: defaultTextStyle,
+                          ),
+                          Text(
+                            '58 %',
+                            style: defaultTextStyle.copyWith(
+                              fontWeight: boldWeight,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'AQ',
+                            style: defaultTextStyle,
+                          ),
+                          Text(
+                            '22',
+                            style: defaultTextStyle.copyWith(
+                              fontWeight: boldWeight,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                CustomContainer(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: defaultMargin,
+                    vertical: defaultMargin,
+                  ),
+                  containerType: RoundedContainerType.noOutline,
+                  radius: defaultRadius,
+                  backgroundColor: AppColor.grey4Color(),
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'SUNRISE & SUNSET',
+                        style: defaultTextStyle,
+                      ),
+                      Image.asset(
+                        'assets/logo.png',
+                        width: 200,
+                      ),
+                      Text(
+                        'Length of day: 13H 12M',
+                        style: defaultTextStyle,
+                      ),
+                      Text(
+                        'Remaining daylight: 9H 22M',
+                        style: defaultTextStyle,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
