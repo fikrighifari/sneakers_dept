057b3814dc1715ed1683466a08a2fc11398012cd
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2023-08-15 17:03:23 +0700

    update: detail movie screen

--- lib/screen/home_screen/detail_movie_screen.dart
+++ lib/screen/home_screen/detail_movie_screen.dart
@@ -1,5 +1,4 @@
 import 'package:sneakers_dept/export.dart';
-import 'package:sneakers_dept/themes/themes.dart';
 import 'package:sneakers_dept/widgets/cards/movie_card.dart';
 import 'package:sneakers_dept/widgets/reusable_components/reusable_components.dart';
 
@@ -19,19 +18,52 @@ class _DetailMovieScreenState extends State<DetailMovieScreen> {
       title: 'Detail',
       child: Column(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(30),
-              bottomRight: Radius.circular(30),
-            ),
-            child: Container(
-              height: MediaQuery.of(context).size.height / 3,
-              width: MediaQuery.of(context).size.width,
-              child: Image.asset(
-                'assets/images/image1.jpeg',
-                fit: BoxFit.cover,
+          Stack(children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+              child: Container(
+                height: MediaQuery.of(context).size.height / 3,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/images/image1.jpeg',
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                MovieCard(imageFileName: 'image2'),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 4,
+                  ),
+                  height: 24,
+                  // width: 54,
+                  color: AppColor.backgroundColor4(),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.orange,
+                      ),
+                      TextWidget(
+                        '9.5',
+                        color: Colors.orange,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ]),
+          TextWidget(
+            'Spiderman No Way Home',
+            color: AppColor.whiteColor(),
           ),
         ],
       ),
