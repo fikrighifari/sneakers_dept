b877633c5c8042348d7098e492613113e4f7a456
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2023-08-15 14:06:36 +0700

    update: routing detail movie screen

--- lib/widgets/cards/movie_card.dart
+++ lib/widgets/cards/movie_card.dart
@@ -1,4 +1,5 @@
 import 'package:sneakers_dept/export.dart';
+import 'package:sneakers_dept/screen/home_screen/detail_movie_screen.dart';
 import 'package:sneakers_dept/widgets/reusable_components/reusable_components.dart';
 
 class MovieCard extends StatelessWidget {
@@ -10,13 +11,23 @@ class MovieCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(16),
-      child: CustomContainer(
-        backgroundColor: AppColor.blackColor(),
-        containerType: RoundedContainerType.noOutline,
-        height: 214,
-        child: Image.asset('assets/images/$imageFileName.jpeg'),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const DetailMovieScreen(),
+          ),
+        );
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: CustomContainer(
+          backgroundColor: AppColor.blackColor(),
+          containerType: RoundedContainerType.noOutline,
+          height: 214,
+          child: Image.asset('assets/images/$imageFileName.jpeg'),
+        ),
       ),
     );
   }
